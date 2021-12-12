dc3ccd853a740babc74df5cd87d4d819b3024964
trif0lium <gh@fleeingsunlight.dev>
2021-12-12 20:35:01 +0700

    fix(docker.sh): /bin/sh -> /busybox/sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 docker run \
   -v $(pwd)/Dockerfile:/kaniko/deploys-dev/Dockerfile \
   -v $(pwd)/build.sh:/kaniko/deploys-dev/build.sh \
-  --entrypoint /bin/sh \
+  --entrypoint /busybox/sh \
   gcr.io/kaniko-project/executor:v1.6.0-debug \
   /kaniko/deploys-dev/build.sh
