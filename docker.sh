6086b071cafc998fe69801f4003cadc40c0e9ebe
trif0lium <gh@fleeingsunlight.dev>
2021-12-12 19:40:53 +0700

    fix(docker.sh): deploys.dev -> kaniko/deploys-dev

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 docker run \
-  -v $(pwd)/Dockerfile:/deploys.dev/Dockerfile \
-  -v $(pwd)/build.sh:/deploys.dev/build.sh \
+  -v $(pwd)/Dockerfile:/kaniko/deploys-dev/Dockerfile \
+  -v $(pwd)/build.sh:/kaniko/deploys-dev/build.sh \
   --entrypoint /bin/sh \
   gcr.io/kaniko-project/executor:v1.6.0-debug \
-  /deploys.dev/build.sh
+  /kaniko/deploys-dev/build.sh
