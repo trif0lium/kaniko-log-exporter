3979abd9d21ec6cdb2273c91bda156008bf40bd2
trif0lium <gh@fleeingsunlight.dev>
2021-12-12 21:39:41 +0700

    chore(build.sh): 2>&1

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
   --log-format=json \
   --verbosity=debug \
   --dockerfile=/kaniko/deploys-dev/Dockerfile \
-  --context=git://github.com/thecodeisalreadydeployed/fixture-nest.git#refs/tags/v2 | tee /kaniko/deploys-dev/kaniko.log && echo "DONE" && cat /kaniko/deploys-dev/kaniko.log
+  --context=git://github.com/thecodeisalreadydeployed/fixture-nest.git#refs/tags/v2 2>&1 | tee /kaniko/deploys-dev/kaniko.log && echo "DONE" && cat /kaniko/deploys-dev/kaniko.log
