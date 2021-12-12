63cdcac1db116f7dcadeda6cd42a1b8c0b2f8f6f
trif0lium <gh@fleeingsunlight.dev>
2021-12-12 18:46:17 +0700

    feat: docker.sh

---
+++ docker.sh
@@ -0,0 +1,5 @@
+docker run \
+  gcr.io/kaniko-project/executor:debug \
+  -v $(pwd)/Dockerfile:/deploys.dev/Dockerfile \
+  -v $(pwd)/build.sh:/deploys.dev/build.sh \
+  --entrypoint /busybox/sh /deploys.dev/build.sh
