c496ac8b6ff925f08084e2e71cd4d5cc3c8e3a71
trif0lium <gh@fleeingsunlight.dev>
2021-12-12 19:08:46 +0700

    feat(build.sh): --no-push

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 /kaniko/executor \
+  --no-push \
+  --log-format=json \
+  --verbosity=debug \
   --dockerfile=/deploys.dev/Dockerfile \
   --context=git://github.com/thecodeisalreadydeployed/fixture-nest.git#refs/tags/v2 | tee /deploys.dev/kaniko.log && echo "DONE" && cat /deploys.dev/kaniko.log
