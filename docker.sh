0c1c84ac74492f5b831355786fb7a1f4637e12f3
trif0lium <gh@fleeingsunlight.dev>
2021-12-12 19:07:18 +0700

    feat(docker.sh): use v1.6.0-debug

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 docker run \
-  gcr.io/kaniko-project/executor:debug \
   -v $(pwd)/Dockerfile:/deploys.dev/Dockerfile \
   -v $(pwd)/build.sh:/deploys.dev/build.sh \
-  --entrypoint "/busybox/sh" /deploys.dev/build.sh
+  --entrypoint /bin/sh \
+  gcr.io/kaniko-project/executor:v1.6.0-debug \
+  /deploys.dev/build.sh
