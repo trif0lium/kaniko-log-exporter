1057812395061bd0402605c593d84bb2f4fbba09
trif0lium <gh@fleeingsunlight.dev>
2021-12-12 19:40:07 +0700

    fix(build.sh): deploys.dev -> kaniko/deploys-dev

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
   --no-push \
   --log-format=json \
   --verbosity=debug \
-  --dockerfile=/deploys.dev/Dockerfile \
-  --context=git://github.com/thecodeisalreadydeployed/fixture-nest.git#refs/tags/v2 | tee /deploys.dev/kaniko.log && echo "DONE" && cat /deploys.dev/kaniko.log
+  --dockerfile=/kaniko/deploys-dev/Dockerfile \
+  --context=git://github.com/thecodeisalreadydeployed/fixture-nest.git#refs/tags/v2 | tee /kaniko/deploys-dev/kaniko.log && echo "DONE" && cat /kaniko/deploys-dev/kaniko.log
